753431e727fbbfab0a6dc32aa8c9beaee021935e
Skysimss <pichard.simon@educ-valadon-limoges.fr>
2023-04-19 15:09:09 +0200

    ajout categorie

--- src/Controller/Admin/CategoriesController.php
+++ src/Controller/Admin/CategoriesController.php
@@ -2,10 +2,19 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Categories;
+use App\Entity\Images;
+use App\Entity\Products;
+use App\Form\CategoriesFormType;
+use App\Form\ProductsFormType;
 use App\Repository\CategoriesRepository;
+use App\Service\PictureService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route('/admin/categories', name: 'admin_categories_')]
 class CategoriesController extends AbstractController
@@ -17,4 +26,46 @@ class CategoriesController extends AbstractController
 
         return $this->render('admin/categories/index.html.twig', compact('categories'));
     }
+
+    #[Route('/ajout', name: 'addcat')]
+    public function add(Request $request, EntityManagerInterface $em, SluggerInterface $slugger, ): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        //On crée une "nouvelle catégorie"
+        $category = new Categories();
+
+        // On crée le formulaire
+        $categoryForm = $this->createForm(CategoriesFormType::class, $category);
+
+
+        // On traite la requête du formulaire
+        $categoryForm->handleRequest($request);
+
+        //On vérifie si le formulaire est soumis ET valide
+        if($categoryForm->isSubmitted() && $categoryForm->isValid()){
+
+            // On génère le slug
+            $slug = $slugger->slug($category->getName());
+            $category->setSlug($slug);
+
+            // On stocke
+            $em->persist($category);
+            $em->flush();
+
+            $this->addFlash('success', 'Catégorie ajouté avec succès');
+
+            // On redirige
+            return $this->redirectToRoute('admin_categories_index');
+        }
+
+
+        // return $this->render('admin/products/add.html.twig',[
+        //     'productForm' => $productForm->createView()
+        // ]);
+
+        return $this->renderForm('admin/categories/add.html.twig', compact('categoryForm'));
+        // ['productForm' => $productForm]
+    }
+
 }
